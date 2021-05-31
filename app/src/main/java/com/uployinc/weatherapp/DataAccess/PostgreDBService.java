ee7912385d06cb47fc985c13cf54171f02a685f0
Georgi Baldjiev <gbaldjiev2004@gmail.com>
2021-06-01 01:00:05 +0300

    db stuff added

---
+++ app/src/main/java/com/uployinc/weatherapp/DataAccess/PostgreDBService.java
@@ -0,0 +1,36 @@
+package com.uployinc.weatherapp.DataAccess;
+
+import android.annotation.SuppressLint;
+
+import com.uployinc.weatherapp.Common.IIndexable;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgreDBService implements IDBService{
+    private Connection connection;
+    private final String hostAddress = "84.40.90.18";
+    private final String databaseName = "weather_api";
+    private final int portNumber = 5432;
+    private final String user = "postgres";
+    private final String pass = "admin";
+    @SuppressLint("DefaultLocale")
+    private final String url = String.format("jdbc:postgresql://%s:%d/%s", this.hostAddress, this.portNumber, this.databaseName);
+
+    public Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection(url, user, pass);
+            } catch (Exception e) {
+                System.out.print(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public <TEntity extends IIndexable<TKey>, TKey> IRepository<TEntity, TKey> GetRepository() {
+        return null;
+    }
+}
