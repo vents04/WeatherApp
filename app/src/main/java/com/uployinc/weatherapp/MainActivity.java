16c2c31374b06ef73e9583515c881585eba95399
vents04 <vencidim04@gmail.com>
2021-05-31 21:15:48 +0300

    View pager and swipe views added. Fragments added.

--- app/src/main/java/com/uployinc/weatherapp/MainActivity.java
+++ app/src/main/java/com/uployinc/weatherapp/MainActivity.java
@@ -1,14 +1,36 @@
 package com.uployinc.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.uployinc.weatherapp.fragments.FragmentLocation;
+import com.uployinc.weatherapp.fragments.FragmentLocationDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new FragmentLocation());
+        fragmentList.add(new FragmentLocationDetails());
+
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new SlidePagerAdapter(getSupportFragmentManager(), fragmentList);
+
+        viewPager.setAdapter(pagerAdapter);
     }
+
 }
---
+++ app/src/main/java/com/uployinc/weatherapp/SlidePagerAdapter.java
@@ -0,0 +1,29 @@
+package com.uployinc.weatherapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class SlidePagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragmentList;
+
+    public SlidePagerAdapter(FragmentManager fragmentManager, List<Fragment> fragmentList) {
+        super(fragmentManager);
+        this.fragmentList = fragmentList;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
