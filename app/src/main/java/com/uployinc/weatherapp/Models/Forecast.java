81bd895ad7f31732a27be3c4b060dd91105426db
Georgi Baldjiev <gbaldjiev2004@gmail.com>
2021-06-01 15:11:05 +0300

    forecast added

--- app/src/main/java/com/uployinc/weatherapp/Models/Forecast.java
+++ app/src/main/java/com/uployinc/weatherapp/Models/Forecast.java
@@ -1,4 +1,17 @@
 package com.uployinc.weatherapp.Models;
 
-public class Forecast {
+import java.time.LocalDateTime;
+
+public abstract class Forecast {
+    protected LocalDateTime targetTime;
+    protected double Temperature;
+    protected double FeelsLike;
+    protected double Pressure;
+    protected double RelativeHumidity;
+    protected int UVIndex;
+    protected double CloudCover;
+    protected double WindSpeed;
+    protected double WindDirection;
+    protected double ProbabilityForPrecipitation;
+    protected double Precipitation;
 }
