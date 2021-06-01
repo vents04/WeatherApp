d622f7c9a5b22ee238d7fcec30e6c1106facb846
vents04 <vencidim04@gmail.com>
2021-06-01 10:49:06 +0300

    Permission check added

--- app/src/main/java/com/uployinc/weatherapp/MainActivity.java
+++ app/src/main/java/com/uployinc/weatherapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.uployinc.weatherapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
@@ -24,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private PagerAdapter pagerAdapter;
+
+    final private int REQUEST_LOCATION = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +52,24 @@ public class MainActivity extends AppCompatActivity {
 
         DotsIndicator dotsIndicator = (DotsIndicator) findViewById(R.id.dots_indicator);
         dotsIndicator.setViewPager(viewPager);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_LOCATION) {
+
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                System.out.println("Location permissions granted, starting location");
+            } else {
+                Log.d("TUKA", "SUM");
+                this.finish();
+                System.exit(0);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
--- app/src/main/java/com/uployinc/weatherapp/fragments/FragmentLocation.java
+++ app/src/main/java/com/uployinc/weatherapp/fragments/FragmentLocation.java
@@ -1,49 +1,87 @@
 package com.uployinc.weatherapp.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.uployinc.weatherapp.MainActivity;
 import com.uployinc.weatherapp.R;
 import com.uployinc.weatherapp.adapters.HoursWeatherRecyclerViewAdapter;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class FragmentLocation extends Fragment {
 
     private ArrayList<String> hours;
     private ArrayList<Bitmap> images;
 
+    private FusedLocationProviderClient fusedLocationClient;
+    final private int REQUEST_LOCATION = 1001;
+    double latitude, longitude;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        hours = new ArrayList<>();
-        hours.add("10:00");
-        hours.add("11:00");
-        hours.add("12:00");
-        hours.add("13:00");
-        hours.add("14:00");
-        hours.add("15:00");
+        initHoursList();
+        initImagesList();
+        getLocation();
+        initHoursWeatherRecyclerView(view);
+    }
 
+    @SuppressLint("MissingPermission")
+    public void getLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+        fusedLocationClient.getLastLocation().addOnSuccessListener((Activity) getContext(), new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                    Log.d("LAT", String.valueOf(latitude));
+                    Log.d("LNG", String.valueOf(longitude));
+                }
+            }
+        });
+    }
+
+    public void initHoursList() {
+        hours = new ArrayList<>();
+        Calendar rightNow = Calendar.getInstance();
+        int currentHourIn24Format = rightNow.get(Calendar.HOUR_OF_DAY);
+        for(int index = currentHourIn24Format; index <= currentHourIn24Format + 24; index++) {
+            hours.add((index%24 < 10) ? "0" + String.valueOf(index%24) + ":00" : String.valueOf(index%24) + ":00");
+        }
+    }
+
+    public void initImagesList() {
         images = new ArrayList<>();
         Bitmap icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.weather_sunny_cloudy_rainy);
-        images.add(icon);
-        images.add(icon);
-        images.add(icon);
-        images.add(icon);
-        images.add(icon);
-        images.add(icon);
-
-        initHoursWeatherRecyclerView(view);
+        for(int index = 0; index <= 24; index++) {
+            images.add(icon);
+        }
     }
 
     private void initHoursWeatherRecyclerView(View view) {
